3a57cffd06613cf2594cba0d3a16760c9d7b0624
AfaqAhmad0 <108234228+AfaqAhmad0@users.noreply.github.com>
2022-12-29 00:13:56 +0500

    artical titles

--- tempall.py
+++ tempall.py
@@ -1,8 +1,8 @@
-
 # Optimized code
-import time,nltk,json,spacy,os
+import time, nltk, json, spacy, os
 
 from nltk.stem import snowball
+
 start_time = time.time()
 # Opening JSON file
 # file = open('C:/Users/HP/Downloads/dataverse_files/json/json/nela-gt-2021/newsdata/abcnews.json')
@@ -12,9 +12,25 @@ text = ""
 words = set()
 
 stop_words = set(nltk.corpus.stopwords.words('english'))
-forward_index = {}
-def merge_indexing( dict2):
-    x = open('C:/Users/HP/PycharmProjects/pythonProject1/filexyz.json','r')
+def inverted_index(forward__,lexicon__):
+    count = 0
+    inverted_index = {}
+    # print(forward__)
+    # print("_______________")
+    # print(lexicon__)
+    for word in lexicon__:
+        inverted_index[word] = {}
+    for artical_id,word_dict in forward__.items():
+        for word in word_dict:
+            # print(artical_id)
+            print(count)
+            temp = {artical_id : forward__[artical_id][word]}
+            inverted_index[word].update(temp)
+        count+=1
+            # print(inverted_index)
+
+def merge_indexing(dict2):
+    x = open('C:/Users/HP/PycharmProjects/pythonProject1/filexyz.json', 'r')
     dict1 = json.load(x)
     x.close()
     for word, articles in dict2.items():
@@ -25,39 +41,63 @@ def merge_indexing( dict2):
     temp = open('C:/Users/HP/PycharmProjects/pythonProject1/filexyz.json', 'w')
     json.dump(dict1, temp)
     file.close()
-def merge_articals( dict2):
-    x = open('C:/Users/HP/Documents/GitHub/main1/atrical_titles.json','r')
-    dict1 = json.load(x)
-    dict1.update(dict2)
-    temp = open('C:/Users/HP/Documents/GitHub/main1/atrical_titles.json', 'w')
-    json.dump(dict1, temp)
-    file.close()
+
+
+# def merge_articals( dict2):
+#     x = open('C:/Users/HP/Documents/GitHub/main1/atrical_titles.json','r')
+#     dict1 = json.load(x)
+#     dict1.update(dict2)
+#     temp = open('C:/Users/HP/Documents/GitHub/main1/atrical_titles.json', 'w')
+#     json.dump(dict1, temp)
+#     file.close()
 
 for temp in os.listdir('C:/Users/HP/Downloads/dataverse_files/json/json/nela-gt-2021/test/'):
-    file = open('C:/Users/HP/Downloads/dataverse_files/json/json/nela-gt-2021/test/'+temp)
+    file = open('C:/Users/HP/Downloads/dataverse_files/json/json/nela-gt-2021/test/' + temp)
     articles = json.load(file)
     print(temp)
     y = 0
-    word_to_docs = {}
-    articles_titles = {}
+    # word_to_docs = {}
+    # articles_titles = {}
+    forward_index = {}
+    lexicon_set = set()
     for article in articles:
-        index = 1
+        forward_index[article['id']] = {}
+        # index = 1
         print(y, end=" ")
-        tokenized_text = [stemmer.stem(word) for word in  nltk.word_tokenize(article['content']) if  word not in stop_words]
-        for word in tokenized_text:
-            tempvar = word_to_docs.get(word, {}).get(article['id'],[0,index])
-            word_to_docs[word] = {article['id'] :  tempvar+[index-tempvar[-1]]}
-            word_to_docs[word][article['id']][0] +=1
-            index+=1
-        y+=1
-        tokenized_title = [stemmer.stem(word) for word in  nltk.word_tokenize(article['title']) if  word not in stop_words]
-        for word in tokenized_title:
-            articles_titles[word] = articles_titles.get(word,[])+[article['id']]
+        tokenized_text = [stemmer.stem(word) for word in nltk.word_tokenize(article['content']) if
+                          word not in stop_words]
+        tokenized_text_set = set(tokenized_text)
+        lexicon_set = lexicon_set.union(tokenized_text_set)
+        for i in tokenized_text_set:
+            forward_index[article['id']][i] = []
+        for word in range(0, len(tokenized_text)):
+            forward_index[article['id']][tokenized_text[word]].append(word)
 
-    merge_indexing(word_to_docs)
-    merge_articals(articles_titles)
+        # print(forward_index)
+        #     tempvar = word_to_docs.get(word, {}).get(article['id'],[0,index])
+        #     tempvar = {article['id'] :tempvar+[index]}
+        #     word_to_docs[word] = {**word_to_docs.get(word,{}),**tempvar}
+        #     word_to_docs[word][article['id']][0] +=1
+        #     index+=1
+        #     # if word in tokenized_title:
+        #     #     word_to_docs[word][article['id']][2] = 1
+        y += 1
+        # {docid: {word:[]}}
 
+        # for word in tokenized_title:
+        #     tempvar = word_to_docs.get(word, {}).get(article['id'],[0,0,0])
+        #     tempvar[2]=1
+        #     tempvar = {article['id']:tempvar}
+        #     word_to_docs[word] = {**word_to_docs.get(word, {}), **tempvar}
+
+        # print(word_to_docs['today'])
+    # merge_indexing(word_to_docs)
+
+    # merge_articals(articles_titles)
+    inverted_index(forward_index,lexicon_set)
     break
+# print(forward_index)
+
 
 end_time = time.time()
 run_time = end_time - start_time
